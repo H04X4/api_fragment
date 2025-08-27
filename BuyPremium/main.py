0081f45ecd8b3123e5c5fee790728af5421ebdbe
UNKNWNCLOWNGHL <58516721+H04X4@users.noreply.github.com>
2025-08-27 19:42:40 +0400

    Update main.py

--- BuyPremium/main.py
+++ BuyPremium/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
-from client import FragmentClient
-from transaction import TonTransaction
+from .client import FragmentClient
+from .transaction import TonTransaction
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 async def main(QUANTITY, QUERY):
@@ -12,7 +12,7 @@ async def main(QUANTITY, QUERY):
         if req_id:
             recipient, amount_nano, la = await client.fetch_buy_link(recipient, req_id, QUERY)
             if recipient and amount_nano and la:
-                amount_decimal = amount_nano / 1_000_000_000
+                amount_decimal = float(amount_nano) / 1_000_000_000
                 logging.info(f"Сумма для отправки: {amount_decimal:.4f} TON")
                 transaction = TonTransaction()
                 await transaction.send_ton_transaction(recipient, amount_decimal, la, QUERY)
@@ -21,4 +21,4 @@ if __name__ == "__main__":
     QUANTITY = 1  
     QUERY = "@tgusername"  
     
-    asyncio.run(main(QUANTITY, QUERY))
+    asyncio.run(main(QUERY, QUANTITY))
