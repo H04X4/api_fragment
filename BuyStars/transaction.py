2a452a85ff6d67a864273c9a33225a1b3df18e39
UNKNWNCLOWNGHL <58516721+H04X4@users.noreply.github.com>
2025-09-20 09:32:13 +0400

    Update transaction.py

--- BuyStars/transaction.py
+++ BuyStars/transaction.py
@@ -14,6 +14,7 @@ def fix_base64_padding(b64_string: str) -> str:
 
 class TonTransaction:
     async def send_ton_transaction(self, recipient, amount_nano, la, stars):
+        # Создаем клиента с API ключом
         client = TonapiClient(api_key=API_TON,
                               is_testnet=False)
 
@@ -21,6 +22,7 @@ class TonTransaction:
 
         logging.info("Кошелек успешно загружен.")
 
+        # Проверяем, что получатель и сумма корректны
         if not recipient:
             logging.error("Ошибка: не указан получатель.")
             return
@@ -28,14 +30,21 @@ class TonTransaction:
             logging.error("Ошибка: некорректная сумма (должна быть больше 0).")
             return
 
+        # Декодирование Base64
+
+        # Исходная строка
         encoded_str = la
 
+        # Декодирование
         decoded_bytes = base64.b64decode(fix_base64_padding(encoded_str))
 
+        # Преобразуем в строку, заменяя нечитаемые символы пробелами
         decoded_text = ''.join(chr(b) if 32 <= b < 127 else ' ' for b in decoded_bytes)
 
+        # Убираем лишние пробелы и переводы строк
         clean_text = re.sub(r'\s+', ' ', decoded_text).strip()
 
+        # Удаляем всё, что идёт до "50 Telegram Stars"
         match = re.search(rf"{stars} Telegram Stars.*", clean_text)
         final_text = match.group(0) if match else clean_text
 
