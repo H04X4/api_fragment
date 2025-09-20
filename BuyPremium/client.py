933873caeeae92e6bb3c342b7f8917b0209287fe
UNKNWNCLOWNGHL <58516721+H04X4@users.noreply.github.com>
2025-09-20 09:31:37 +0400

    Update client.py

--- BuyPremium/client.py
+++ BuyPremium/client.py
@@ -13,22 +13,22 @@ def get_cookies(DATA):
 class FragmentClient:
     URL = F"https://fragment.com/api?hash={FRAGMENT_HASH}"
 
-    async def fetch_recipient(self, query):
-        data = {"query": query,  "months": 3, "method": "searchPremiumGiftRecipient"}
-   
+    async def fetch_recipient(self, query, mountity):
+        data = {"query": query,  "months": mountity, "method": "searchPremiumGiftRecipient"}
+        print(data)
         async with httpx.AsyncClient() as client:
             response = await client.post(self.URL, cookies=get_cookies(DATA), data=data)
             print(response.json())
             return response.json().get("found", {}).get("recipient")
 
-    async def fetch_req_id(self, recipient, quantity):
-        data = {"recipient": recipient, "months": 3, "method": "initGiftPremiumRequest"}
+    async def fetch_req_id(self, recipient, mountity):
+        data = {"recipient": recipient, "months": mountity, "method": "initGiftPremiumRequest"}
         async with httpx.AsyncClient() as client:
             response = await client.post(self.URL, cookies=get_cookies(DATA), data=data)
             print(response.json())
             return response.json().get("req_id")
 
-    async def fetch_buy_link(self, recipient, req_id, quantity):
+    async def fetch_buy_link(self, recipient, req_id, mountity):
         data = {
             "address": f"{FRAGMENT_ADDRES}", "chain": "-239",
             "walletStateInit": f"{FRAGMENT_WALLETS}",
@@ -44,16 +44,15 @@ class FragmentClient:
             "accept": "application/json, text/javascript, */*; q=0.01",
             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
             "origin": "https://fragment.com",
-            "referer": f"https://fragment.com/premium/gift?recipient={recipient}&months=3",
+            "referer": f"https://fragment.com/premium/gift?recipient={recipient}&months={mountity}",
             "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
             "x-requested-with": "XMLHttpRequest"
         }
         async with httpx.AsyncClient() as client:
             response = await client.post(self.URL, headers=headers, cookies=get_cookies(DATA), data=data)
             json_data = response.json()
-            print(response.json())
+            print(json_data)
             if json_data.get("ok") and "transaction" in json_data:
                 transaction = json_data["transaction"]
                 return transaction["messages"][0]["address"], transaction["messages"][0]["amount"], transaction["messages"][0]["payload"]
         return None, None, None
-
