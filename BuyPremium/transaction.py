11916d3e108896444f5493c9ff6ba3c365e9ed3a
UNKNWNCLOWNGHL <58516721+H04X4@users.noreply.github.com>
2025-09-20 09:30:31 +0400

    Update transaction.py

--- BuyPremium/transaction.py
+++ BuyPremium/transaction.py
@@ -7,18 +7,17 @@ from tonutils.wallet import WalletV5R1
 from config import API_TON, MNEMONIC
 
 def fix_base64_padding(b64_string: str) -> str:
+    """Добавляет недостающие символы '=' в base64 строку."""
     missing_padding = len(b64_string) % 4
     if missing_padding:
         b64_string += '=' * (4 - missing_padding)
     return b64_string
 
 class TonTransaction:
-    async def send_ton_transaction(self, recipient, amount_nano, la, stars):
-        client = TonapiClient(api_key=API_TON,
-                              is_testnet=False)
-
+    async def send_ton_transaction(self, recipient, amount_nano, la, stars=None):
+        """Отправка TON с текстом подарка Telegram Premium."""
+        client = TonapiClient(api_key=API_TON, is_testnet=False)
         wallet, public_key, private_key, mnemonic = WalletV5R1.from_mnemonic(client, MNEMONIC)
-
         logging.info("Кошелек успешно загружен.")
 
         if not recipient:
@@ -27,18 +26,16 @@ class TonTransaction:
         if amount_nano <= 0:
             logging.error("Ошибка: некорректная сумма (должна быть больше 0).")
             return
-        encoded_str = la
 
-
-        decoded_bytes = base64.b64decode(fix_base64_padding(encoded_str))
-
+        decoded_bytes = base64.b64decode(fix_base64_padding(la))
         decoded_text = ''.join(chr(b) if 32 <= b < 127 else ' ' for b in decoded_bytes)
-
         clean_text = re.sub(r'\s+', ' ', decoded_text).strip()
 
-        match = re.search(rf"Telegram.*", clean_text)
-        final_text = match.group(0) if match else clean_text
+        match = re.search(r'(Telegram.*?Ref\s*#\S+)', clean_text)
+        final_text = match.group(1).replace('Ref #', 'Ref#') if match else clean_text
+        print(final_text)
 
+        logging.info(f"Формируем текст для транзакции: {final_text}")
 
         tx_hash = await wallet.transfer(
             destination=recipient,
@@ -46,8 +43,3 @@ class TonTransaction:
             body=final_text,
         )
         logging.info(f"✅ Транзакция отправлена: {tx_hash}")
-
-
-
-
-
